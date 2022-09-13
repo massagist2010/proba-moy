6cc84796a0680e058ea118b52a383c77521b32e2
massagist2010 <rusy_boob2@mail.ru>
2022-09-13 21:51:11 +0300

    я тренеруюсь с веткой пробной

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
     int    httpsmaster = 33;
         System.out.println(httpsmaster + " i'm trenninger");
+        byte proba = 127;
+        double raznica = httpsmaster % proba;
+        System.out.println(raznica);
+
     }
 }
